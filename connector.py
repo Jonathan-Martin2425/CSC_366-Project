2487af00b52f29758a6ac28e788fb3c37586b700
Jonathan-Martin2425 <jonathan.martin2425@gmail.com>
2026-03-03 14:05:18 -0800

    connecter.py finish

---
+++ connector.py
@@ -0,0 +1,30 @@
+import mysql.connector
+import json
+
+
+# tries to connect to the host using data found in
+# the given file that has everything needed
+# to connect to the DB
+# returns a DB/SQL connection object
+def make_connection(file):
+    with open(file, "r") as settings:
+        d = json.load(settings)
+        # get data from settings.config, then connect to DB
+        return mysql.connector.connect(
+            host=d["host"],
+            user=d["user"],
+            password=d["password"],
+            port=d["port"],
+            database=d["database"],
+        )
+
+
+if __name__ == "__main__":
+    DB = make_connection("settings.config")
+    cursor = DB.cursor()
+
+    cursor.execute('SHOW TABLES')
+    tables = cursor.fetchall()
+
+    for table in tables:
+        print(table)
