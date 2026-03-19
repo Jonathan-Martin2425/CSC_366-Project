e6080b3e0484b8210bf990c62e2eccfaa1710fd0
Davis Morales <davismorales38@gmail.com>
2026-03-18 17:23:48 -0700

    Updated floorplan api calls

--- floorplan_api.py
+++ floorplan_api.py
@@ -2,9 +2,11 @@ from connector import make_connection
 from permissions import check_permission
 
 def getFloorplans():
+    # Connect to the database using the credentials
     DB = make_connection("settings.config")
     cursor = DB.cursor()
 
+    # The SQL query that'll be executed
     query = """
         SELECT
             FLOORPLANS.FImagePath,
@@ -16,26 +18,26 @@ def getFloorplans():
         ON FLOORPLANS.BNumber = BUILDINGS.BNumber
     """
 
+    # Execute the SQL query on the database
     cursor.execute(query)
     results = cursor.fetchall()
 
-    floorplans = []
+    # Close the database connection
+    cursor.close()
+    DB.close()
 
-    for uri, building_name, building_number, floor_number in results:
-        floorplans.append({
+    # Format the results to be readable and return them
+    return [
+        {
             "URI": uri,
             "BuildingName": building_name,
             "BuildingNumber": building_number,
             "FloorNumber": floor_number
-        })
-
-    cursor.close()
-    DB.close()
-
-    return floorplans
+        }
+        for uri, building_name, building_number, floor_number in results
+    ]
 
 if __name__ == "__main__":
     plans = getFloorplans()
-
     for plan in plans:
         print(plan)
